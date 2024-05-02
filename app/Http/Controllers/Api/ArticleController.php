9b3b33e50337743436cfc94165f7b519bacd197a
小光 <leo155186@gmail.com>
2024-05-02 11:43:24 +0800

    feat：新增swagger文件註解

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -9,6 +9,14 @@ use App\Services\ArticleService;
 
 class ArticleController extends Controller
 {
+    /**
+     * @OA\Info(
+     *     title="Your API Title",
+     *     version="1.0.0",
+     *     description="Your API Description"
+     * )
+     */
+
     private $articleService;
 
     public function __construct(ArticleService $service)
@@ -21,6 +29,49 @@ class ArticleController extends Controller
      * @param  ArticleRequest  $request
      *
      */
+    /**
+     * @OA\Post(
+     *     path="/api/post/article",
+     *     summary="Create a new article",
+     *     tags={"postArticle"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"id", "language_code", "title", "content"},
+     *             @OA\Property(property="id", type="string", example="Sample ID"),
+     *             @OA\Property(property="language_code", type="string", example="en"),
+     *             @OA\Property(property="title", type="string", example="Sample Title"),
+     *             @OA\Property(property="content", type="string", example="Sample Content")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Article created successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Article created successfully")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="The given data was invalid"),
+     *             @OA\Property(property="errors", type="object", example={
+     *                 "id": {"Article ID is required."},
+     *                 "id": {"Article ID must be a numeric."},
+     *                 "language_code": {"Language code is required."},
+     *                 "language_code": {"Language code must be a string."},
+     *                 "language_code": {"language code must be either `en`, `zh`, or `ja`."},
+     *                 "title": {"Title is required."},
+     *                 "title": {"Title must be a string."},
+     *                 "title": {"Title may not be greater than 20 characters."},
+     *                 "content": {"Content is required."},
+     *                 "content": {"Content must be a string."},
+     *             })
+     *         )
+     *     ),
+     * )
+     */
     public function postArticle(ArticleRequest $request)
     {
         return $this->articleService->store($request);     
@@ -31,6 +82,39 @@ class ArticleController extends Controller
      * @param  ArticleRequest  $request
      *
      */
+    /**
+     * @OA\Post(
+     *     path="/api/delete/article",
+     *     summary="Delete a new article",
+     *     tags={"deleteArticle"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"id"},
+     *             @OA\Property(property="id", type="string", example="Sample ID"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Article deleted successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Article deleted successfully")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="The given data was invalid"),
+     *             @OA\Property(property="errors", type="object", example={
+     *                 "id": {"Article ID is required."},
+     *                 "id": {"Article ID must be a numeric."},
+     *                 "id": {"Article ID does not exist."},
+     *             })
+     *         )
+     *     ),
+     * )
+     */
     public function deleteArticle(ArticleRequest $request)
     {
         return $this->articleService->delete($request);      
@@ -40,6 +124,39 @@ class ArticleController extends Controller
      * 查詢文章
      * @param  ArticleRequest  $request
      *
+     */
+    /**
+     * @OA\Post(
+     *     path="/api/select/article",
+     *     summary="Selected a new article",
+     *     tags={"selectArticle"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"id"},
+     *             @OA\Property(property="id", type="string", example="Sample ID"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Article selected successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Article selected successfully")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="The given data was invalid"),
+     *             @OA\Property(property="errors", type="object", example={
+     *                 "id": {"Article ID is required."},
+     *                 "id": {"Article ID must be a numeric."},
+     *                 "id": {"Article ID does not exist."},
+     *             })
+     *         )
+     *     ),
+     * )
      */
     public function selectArticle(ArticleRequest $request)
     {
