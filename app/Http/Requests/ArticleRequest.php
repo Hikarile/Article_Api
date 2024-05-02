1f308afed438cdc26f3f64c28ca4badef426744e
小光 <leo155186@gmail.com>
2024-05-02 10:29:38 +0800

    feat：新增 自訂義ArticleRequest

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ArticleRequest extends FormRequest
+{
+    public function rules() : array
+    {
+        switch ($this->method()) {
+            case 'GET':
+                return [
+                    'id'            => 'required|numeric|exists:articles,id',
+                ];
+            case 'POST':
+                return [
+                    'id'            => 'required|numeric',
+                    'language_code' => [
+                        'required',
+                        'string',
+                        'regex:/^(en|zh|ja)$/'
+                    ],
+                    'title'         => 'required|max:20',
+                    'content'       => 'required|string',
+                ];
+            case 'DELETE':
+                return [
+                    'id'            => 'required|numeric|exists:articles,id',
+                ];
+        }
+    }
+
+    public function messages()
+    {
+        return [
+            'id.required' => 'Article ID is required.',
+            'id.exists' => 'Article does not exist.',
+            'language_code.required' => 'Language code is required.',
+            'language_code.string' => 'Language code must be a string.',
+            'language_code.regex' => 'language code must be either "en", "zh", or "ja".',
+            'title.required' => 'Title is required.',
+            'title.string' => 'Title must be a string.',
+            'title.max' => 'Title may not be greater than 20 characters.',
+            'content.required' => 'Content is required.',
+            'content.string' => 'Content must be a string.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors()
+        ], 422));
+    }
+    
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
