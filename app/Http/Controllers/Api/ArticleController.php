6da0d4d3c2e33bcc5e408afa246e60566d8ed4e6
小光 <leo155186@gmail.com>
2024-05-02 10:31:06 +0800

    feat：新增 ArticleController

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -4,36 +4,45 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+use App\Services\ArticleService;
 
 class ArticleController extends Controller
 {
+    private $articleService;
+
+    public function __construct(ArticleService $service)
+    {
+        $this->articleService = $service;
+    }
+
     /**
      * 新增文章
-     * @param  Request  $request  :{"StoreId": "111119",“OrderNo": QRCode中的唯一值}
+     * @param  ArticleRequest  $request
      *
      */
-    public function postArticle(Request $request)
+    public function postArticle(ArticleRequest $request)
     {
-        return 'postArticle';        
+        return $this->articleService->store($request);     
     }
 
     /**
      * 刪除文章
-     * @param  Request  $request  :{"StoreId": "111119",“OrderNo": QRCode中的唯一值}
+     * @param  ArticleRequest  $request
      *
      */
-    public function deleteArticle(Request $request)
+    public function deleteArticle(ArticleRequest $request)
     {
-        return 'deleteArticle';        
+        return $this->articleService->delete($request);      
     }
 
     /**
      * 查詢文章
-     * @param  Request  $request  :{"StoreId": "111119",“OrderNo": QRCode中的唯一值}
+     * @param  ArticleRequest  $request
      *
      */
-    public function getArticle(Request $request)
+    public function selectArticle(ArticleRequest $request)
     {
-        return 'getArticle';        
+        return $this->articleService->select($request);        
     }
 }
